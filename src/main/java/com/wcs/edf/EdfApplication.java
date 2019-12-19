392d3b96c8463022c991f61886874066350f1856
Laurent Rous <rouslaurent@gmail.com>
2019-12-19 13:32:49 +0100

    first

---
+++ src/main/java/com/wcs/edf/EdfApplication.java
@@ -0,0 +1,13 @@
+package com.wcs.edf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdfApplication.class, args);
+	}
+
+}
